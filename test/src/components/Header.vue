10b4bc843ec0d15d825d8f2b5b22f2a091c40fce
haozi <CCTV@qq.com>
2021-03-15 21:08:27 +0800

    ahead

---
+++ test/src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="Header">
+		<div class = "header-left">
+			<span class = "iconfont">&#xe7bf;</span>
+			<!--此处插入icon为网上引入-->
+		</div>
+		<div class = "header-input">
+			<span class = "iconfont">&#xe7be;</span>
+			内容/内容
+		</div>
+		<div class = "header-right">
+			城市
+			<span class = "iconfont litleChange">&#xe7c0;</span>
+		</div>
+		<!--i like Vue测试成功-->
+	</div>
+</template>
+
+<script>
+
+	export default {
+  	name: 'Header',
+ };
+ 
+</script>
+
+
+<!--lang = "stylus"scoped-->
+<style>
+	@import url("../assets/styles/varibles.styl");/*引入全局样式*/
+	.Header{
+	display:flex;
+	height: .86rem;
+	background:#00bcd4;
+	color:#fff;
+	}
+	.header-left{
+	padding-left:.2rem;
+	width: .3rem;
+	float: left;
+	margin-top:.25rem;
+	}
+	.header-input{
+	padding-left: .1rem;
+	flex:1;
+	height:.64rem;
+	line-height:.64rem;
+	margin-top:.1rem;
+	margin-left:.2rem;
+	background :#fff;
+	border-radius:.18rem;
+	color:#ccc;
+	}
+	.header-right{
+	width:1.2rem;
+	float:right;
+	text-align:center;
+	margin-top:.25rem;
+		.litleChange{
+			padding-left: .5rem;
+		}
+	}
+</style>
--- test/src/main.js
+++ test/src/main.js
@@ -6,7 +6,7 @@ import router from './router'//寻找router文件下叫router的组件
 import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
-
+import './assets/styles/iconfont.css'//添加icon
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 

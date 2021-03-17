be96a4472a71a803e9b224d1f2df3468278e203c
haozi <CCTV@qq.com>
2021-03-17 21:12:05 +0800

    add 3.17 renew0.2

--- test/src/components/Home.vue
+++ test/src/components/Home.vue
@@ -3,17 +3,20 @@
 	<div>
 		<home-header></home-header>
 		<home-swiper></home-swiper>
+		<home-icons></home-icons>
 	</div>
 </template>
 
 <script>
 	import HomeHeader from './Header'
 	import HomeSwiper from './Swiper'
+	import HomeIcons from './Icons'
 	export default{
 		name:'Home',
 		components:{
 			HomeHeader,
-			HomeSwiper
+			HomeSwiper,
+			HomeIcons
 		}
 	}
 </script>
---
+++ test/src/components/Icons.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class = 'icons'>
+				<div class = 'icon'>
+					<img class = 'icon-img' src = "https://pr1-greenteacdn.lenovo.com.cn/config/1606990758564_72x72_看图王.png"/>
+					
+		
+		</div>
+				<!--<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>
+				<div class = 'icon'>
+		
+		</div>-->
+		
+	
+	</div>
+</template>
+
+<script>
+		export default{
+		name:'HomeIcons',
+		data(){
+	
+		}
+	}
+</script>
+
+<style>
+	.icons{
+		overflow : hidden;
+		height : 0;
+		padding-bottom: 50%;
+		background: darkgreen;
+	}
+	.icon{
+		float :left;
+		width :25%;
+		padding-bottom: 25%;s
+		background: red;
+	}
+	.icon-img{
+		position: absolute;
+		top :0;
+		left :0;
+		right :0;
+		bottom :.44rem;
+		box-sizing: border-box;
+		padding: .1rem;
+		background: blue;
+		
+	}
+</style>

fc6f56c106410b1309a8b8ae0571ea5aa46edf2a
haozi <CCTV@qq.com>
2021-03-17 20:35:04 +0800

    add 3.17 renew0.1

--- test/src/components/Swiper.vue
+++ test/src/components/Swiper.vue
@@ -1,20 +1,31 @@
 <!--新增3.17-->
 <template>
+	 <div class = "flesh">
+	 	<!--div的位置会影响滚动-->
 	<swiper :options="swiperOption" >
     <!-- slides -->
-		<swiper-slide>I'm Slide 1</swiper-slide>
-		<swiper-slide>I'm Slide 2</swiper-slide>
-		<swiper-slide>I'm Slide 3</swiper-slide>
-		<swiper-slide>I'm Slide 4</swiper-slide>
-		<swiper-slide>I'm Slide 5</swiper-slide>
-		<swiper-slide>I'm Slide 6</swiper-slide>
-		<swiper-slide>I'm Slide 7</swiper-slide>
-		<!-- Optional controls -->
-		<div class="swiper-pagination"  slot="pagination"></div>
-		<div class="swiper-button-prev" slot="button-prev"></div>
-		<div class="swiper-button-next" slot="button-next"></div>
-		<div class="swiper-scrollbar"   slot="scrollbar"></div>
+	   
+			<swiper-slide>
+				<img src = "https://webstatic.mihoyo.com/ys/event/e20210317-fab/images/mask.e3f09839.png" class = "photo">
+			</swiper-slide>
+	   
+			<!--轮播内容-->
+			<swiper-slide>
+				<img src = "https://webstatic.mihoyo.com/ys/event/e20210317-fab/images/wd_poster.62ee06c2.png" class = "photo">
+			</swiper-slide>
+			<!--<swiper-slide>I'm Slide 3</swiper-slide>
+			<swiper-slide>I'm Slide 4</swiper-slide>
+			<swiper-slide>I'm Slide 5</swiper-slide>
+			<swiper-slide>I'm Slide 6</swiper-slide>
+			<swiper-slide>I'm Slide 7</swiper-slide>-->
+			<!-- Optional controls -->
+			<div class="swiper-pagination"  slot="pagination"></div>
+			<!--<div class="swiper-button-prev" slot="button-prev"></div>
+			<div class="swiper-button-next" slot="button-next">不需要箭头</div>-->
+			<!--<div class="swiper-scrollbar"   slot="scrollbar">不需要滚动条</div>-->
+		 
   </swiper >
+	 </div>
 </template>
 
 <script>
@@ -22,7 +33,10 @@
 		name:'HomeSwiper',
 		data(){
 			return{
-				swiperOption:{}
+				swiperOption:{
+					pagination:'.swiper-pagination',
+					loop:true
+				}
 			}
 		}
 	}
@@ -30,4 +44,18 @@
 
 <style>
 	 /*lang = "stylus" scoped*/
+	.photo{
+	/*	overflow:hidden;*/
+		width: 100%;
+		
+	}
+	.flash{
+		overflow:hidden;
+		width: 100%;
+		height:0 ;
+		padding-bottom: 32.5%;
+	}
+	.flash >>> .swiper-pagination-bullets-active{
+		background: #FFFFFF ; 
+	}
 </style>

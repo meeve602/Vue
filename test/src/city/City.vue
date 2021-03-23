1f7cd71a4edf6f6b516fc095ce9518dd7f3ba24a
haozi <CCTV@qq.com>
2021-03-23 21:09:21 +0800

    add 3.23 renew0.2

--- test/src/city/City.vue
+++ test/src/city/City.vue
@@ -1,15 +1,20 @@
 <template>
 	<div>
+		<!--关联组件不分大小写，可以有“-”号，但名字必须匹配-->
 		<city-header></city-header>
+		<Search></Search>
 	</div>
 </template>
 
 <script>
 	import CityHeader from './CityHeader'
+	import Search from './Search'
+	
 	export default {
   		name: 'City',
   		components: {
 			CityHeader,
+			Search,
   		}
  	};
 </script>
---
+++ test/src/city/Search.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class = "search">
+		<input class = "search-input" type = "text" placeholder="输入城市名"/>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Search',
+	};
+</script>
+
+<style>
+	.search{
+		height : .72rem;
+		padding : .1rem;
+		background: #00BCD4;
+	}
+	.search-input{
+		box-sizing: border-box;
+		width: 100%;
+		height :.62rem;
+		padding: .1rem;
+		line-height: .62rem;
+		text-align: center;
+		border-radius: .06rem;
+		color: #666;
+	}
+</style>

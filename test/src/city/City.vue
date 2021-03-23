baa3672776c8b956d087253682a704c449f8ecd5
haozi <CCTV@qq.com>
2021-03-23 21:30:53 +0800

    add 3.23 renew0.3

--- test/src/city/City.vue
+++ test/src/city/City.vue
@@ -3,18 +3,21 @@
 		<!--关联组件不分大小写，可以有“-”号，但名字必须匹配-->
 		<city-header></city-header>
 		<Search></Search>
+		<List></List>
 	</div>
 </template>
 
 <script>
 	import CityHeader from './CityHeader'
 	import Search from './Search'
+	import List from './List'
 	
 	export default {
   		name: 'City',
   		components: {
 			CityHeader,
 			Search,
+			List,
   		}
  	};
 </script>

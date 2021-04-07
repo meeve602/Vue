fc2c839e1dda730a4ab7ef9259f726effa8ea65c
haozi <CCTV@qq.com>
2021-04-07 20:29:53 +0800

    add 4.7 renew0.1

--- test/src/detail/Detail.vue
+++ test/src/detail/Detail.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
 		<detail-banner></detail-banner>
+		<detail-header></detail-header>
 	</div>
 </template>
 
 <script>
 	import detailBanner from './Banner'
+	import detailHeader from './DetailHeader'
 	export default{
 		name:'Detail',
 		components: {
 			detailBanner,
+			detailHeader,
 		},
 	}
 </script>

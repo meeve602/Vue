73c5ddefebf4bf5521a0c66f3ad1f206c85c3f07
haozi <CCTV@qq.com>
2021-04-07 21:06:22 +0800

    add 4.7 renew0.3

--- test/src/detail/Detail.vue
+++ test/src/detail/Detail.vue
@@ -2,18 +2,28 @@
 	<div>
 		<detail-banner></detail-banner>
 		<detail-header></detail-header>
+		<div class="content">
+		<detail-list :list="list"></detail-list>
+		</div>
 	</div>
 </template>
 
 <script>
 	import detailBanner from './Banner'
 	import detailHeader from './DetailHeader'
+	import detailList from './DetailList'
 	export default{
 		name:'Detail',
 		components: {
 			detailBanner,
 			detailHeader,
+			detailList,
 		},
+		data () {
+	    return {
+	      	list: []
+    }
+  },
 	}
 </script>
 
